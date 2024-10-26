85cf2155ae2487c80b512994930db60368e0c907
Michael Panzer <michaelpanzer04@gmail.com>
2024-10-26 13:17:41 -1000

    Added my own matrix block function

--- Five_Link.py
+++ Five_Link.py
@@ -1,28 +1,48 @@
 import numpy as np
 
 
+#figure out a better way to do this np.block?
+def __block__(matrix):
+    block_shape = matrix[0][0].shape
+    output = np.zeros((matrix.shape[0]*block_shape[0], matrix.shape[1]*block_shape[1]))
+
+    for i, row in enumerate(matrix):
+        for j, block in enumerate(row):
+            for k, block_row in enumerate(block):
+                for l, val in enumerate(block_row):
+                    output[i*block_shape[0]+k][j*block_shape[1]+l] = val
+
+    return output
+
 
 def __gen_P_R__(upright_pickups):
-    p_r = np.zeros((5,3))
+    p_r = np.zeros((5,3), dtype=np.ndarray)
+    
 
-    for pickup_n in upright_pickups:
-        print(pickup_n)
 
-    for n in p_r:
-        print(n)
+    for i, pickup_n in enumerate(upright_pickups) :
+        for j, pickup_coord in enumerate(pickup_n):
+            p_r[i, j] = pickup_coord*np.identity(3)
 
-    for pickup_n, p_r_row in upright_pickups, p_r:
-        for pickup_coord, p_r_index in pickup_n, p_r_row:
-            p_r_index = pickup_coord*np.identity(3)
-
-    return p_r
+    return __block__(p_r)
 
 
 mat = __gen_P_R__(np.array([[1,2,3],
                     [1,2,3],
                     [1,2,3],
                     [1,2,3],
-                    [1,2,3],]))
+                    [1,2,3]]))
 
+print(mat.shape)
+
+print("\n\n\n")
 print(mat)
+
+#block1 = np.array([[1,2], [3,4]])
+#block2 = np.array([[5,6], [7,8]])
+
+
+#print(mat)
+#print("\n\n")
+#print(np.concatenate(mat, axis = 1))
         
