a3846442eb095c0dfaf6f9f2320614e9c3b4b637
Michael Panzer <michaelpanzer04@gmail.com>
2024-11-02 12:56:54 -1000

    wrote tests for matrix generation functions

--- Five_Link.py
+++ Five_Link.py
@@ -36,7 +36,7 @@ def __block__(matrix):
 def __gen_P_W__():
     #output = np.zeros((5,1), dtype=np.ndarray)
 
-    output = [[-1*np.identity]*1]*5
+    output = [[np.identity(3)]*1]*5
 
     #for i, output_val in enumerate(output):
         #output[i, 0] = -1*np.identity(3)
@@ -47,13 +47,13 @@ def __gen_P_W__():
 
 def __gen_P_R__(upright_pickups):
     #p_r = np.zeros((5,3), dtype=np.ndarray)
-    p_r = [[np.identity] * 3] *5
+    p_r = [[np.identity(3)] * 3] *5
 
 
     #each block is a pickup coordinate * the identity matrix
     for i, pickup_n in enumerate(upright_pickups) :
         for j, pickup_coord in enumerate(pickup_n):
-            p_r[i, j] *= pickup_coord
+            p_r[i][j] *= pickup_coord
 
     #combines the block matrix
     return np.block(p_r)
@@ -64,7 +64,7 @@ def __gen_P_AB__(lengths):
 
     for i, length in enumerate(lengths):
         for j in range(3*i, 3*i+3):
-            output[j] = length
+            output[j] = -1*length
 
     return np.diag(output)
 
