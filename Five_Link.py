995c232f78e696f342c1bae76e9209e7bb2ed1ea
Michael Panzer <michaelpanzer04@gmail.com>
2024-11-09 15:40:53 -1000

    changed link angles so that beta=0 means link is perpendicual to z axis

--- Five_Link.py
+++ Five_Link.py
@@ -19,6 +19,9 @@ class Five_Link:
         self.frame_pickups = frame_pickups
         self.upright_pickups = upright_pickups
 
+        self.P = self.gen_P()
+        self.A = self.gen_A()
+
     def __init__(self, file_name):
         
         self.link_lengths = np.zeros(5)
@@ -77,11 +80,11 @@ class Five_Link:
         # swap colums arorund
         return np.diag(output)
 
-    def gen_P(upright_pickups, lengths):
-        return np.block([__gen_P_W__(), __gen_P_R__(upright_pickups), __gen_P_AB__(lengths)])
+    def gen_P(self):
+        return np.block([self.__gen_P_W__(), self.__gen_P_R__(self.upright_pickups), self.__gen_P_AB__(self.link_lengths)])
 
-    def gen_A(frame_pickups):    
-        return np.atleast_2d(np.ravel(frame_pickups)).T
+    def gen_A(self):    
+        return np.atleast_2d(np.ravel(self.frame_pickups)).T
 
     def solve_linear(P, A):
         #driving_variable = variable_index_lookup[driving_variable]
@@ -102,12 +105,11 @@ class Five_Link:
     def gen_AB_nonlinear_eq(vars):
         alpha, beta = vars
     
-        AB_x = sympy.cos(alpha) * sympy.sin(beta)
-        AB_y = sympy.sin(alpha) * sympy.sin(beta)
-        AB_z = sympy.cos(beta)
+        AB_x = sympy.cos(alpha) * sympy.cos(beta)
+        AB_y = sympy.sin(alpha) * sympy.cos(beta)
+        AB_z = sympy.sin(beta)
         
         return AB_x, AB_y, AB_z
-
 
     def gen_R_nonlinear_eq(vars):
         theta, phi, gamma = vars
@@ -126,35 +128,12 @@ class Five_Link:
 
         return r.T.flatten()
 
-
-    def gen_AB_nonlinear_eq(vars):
-        AB_x, AB_y, AB_z = vars
-
-        return AB_x**2 + AB_y**2 + AB_z**2 - 1
-
-
-    def gen_R_nonlinear_eq(vars):
-        r_0,r_1,r_2,r_3,r_4,r_5,r_6,r_7,r_8 = vars
-
-
-        #this comes from the R*Rt = I
-        eq_0 = r_0**2 + r_3**2 + r_6**2 - 1
-        eq_1 = r_1**2 + r_4**2 + r_7**2 - 1
-        eq_2 = r_2**2 + r_5**2 + r_8**2 - 1
-
-        eq_3 = r_0*r_1 + r_3*r_4 + r_6*r_7
-        eq_4 = r_0*r_2 + r_3*r_5 + r_6*r_8
-        eq_5 = r_1*r_2 + r_4*r_5 + r_7*r_8
-
-        return np.array([eq_0, eq_1, eq_2, eq_3, eq_4, eq_5])
-
-    def gen_systems_of_equations(x, P, A, driving_var, value):
-        #x_linear = np.array(sympy.symbols("w_x w_y w_z r_0 r_1 r_2 r_3 r_4 r_5 r_6 r_7 r_8 ab_x_0, ab_y_0, ab_z_0 ab_x_1 ab_y_1 ab_z_1 ab_x_2 ab_y_2 ab_z_2 ab_x_3 ab_y_3 ab_z_3 ab_x_4 ab_y_4 ab_z_4"))
+    def gen_systems_of_equations(self, x, driving_var, value):
         x_linear = np.zeros(27)
         
         x_linear[0:3] = x[0:3]
 
-        x_linear[3:12] = gen_R_nonlinear_eq(x[3:6])
+        x_linear[3:12] = self.gen_R_nonlinear_eq(x[3:6])
         #print(x_linear)
         #print("\n")
 
@@ -162,13 +141,13 @@ class Five_Link:
         ab_angles = x[6:]
 
         for i in range(5):
-            ab_vecs[3*i:3*i+3] = gen_AB_nonlinear_eq((ab_angles[2*i], ab_angles[2*i+1]))
+            ab_vecs[3*i:3*i+3] = self.gen_AB_nonlinear_eq((ab_angles[2*i], ab_angles[2*i+1]))
         
         #print(x_linear)
         #print("\n")
 
         
-        linear_equations = ((np.dot(P, x_linear)).T - A.T)[0]
+        linear_equations = ((np.dot(self.P, x_linear)).T - self.A.T)[0]
 
         driving_expression = x[driving_var] - value
 
--- main.py
+++ main.py
@@ -33,8 +33,7 @@ P_mat = fl.gen_P(upright_pickups, link_lengths)
 
 A_vec = fl.gen_A(frame_pickups)
 
-#x = np.array(sympy.symbols("w_x w_y w_z r_0 r_1 r_2 r_3 r_4 r_5 r_6 r_7 r_8 ab_x_0, ab_y_0, ab_z_0 ab_x_1 ab_y_1 ab_z_1 ab_x_2 ab_y_2 ab_z_2 ab_x_3 ab_y_3 ab_z_3 ab_x_4 ab_y_4 ab_z_4"))
-#x = np.array(sympy.symbols("w_x w_y w_z theta phi gamma alpha_0 beta_0 alpha_1 beta_1 alpha_2 beta_2 alpha_3 beta_3 alpha_4 beta_4"))
+five_link = fl.Five_Link(link_lengths, frame_pickups, upright_pickups)
 
 
 
