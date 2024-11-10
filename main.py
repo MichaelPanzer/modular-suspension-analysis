42a4fad4760d71193abe3e8ea0dbe223d3417328
Michael Panzer <michaelpanzer04@gmail.com>
2024-11-09 16:31:35 -1000

    Added function to create table of outputs

--- main.py
+++ main.py
@@ -33,28 +33,35 @@ five_link = fl.Five_Link(link_lengths, frame_pickups, upright_pickups)
 
 #print(equations(x, x[2]-69))
 
-def solution(z, system):
-    x_0 = np.array([5,0,0,
-                0,0,0,
-                0.00,0.00,
-                0.00,0.00,
-                0.00,0.00,
-                0.00,0.00,
-                0.00,0.00])
-    
+#solves suspension kinematics system of equations in terms of z
+def solve_z(z, guess, system):
     driving_var = 2
     
-    def system(vars):
-        return five_link.gen_systems_of_equations(vars, driving_var, z)
+    def function(vars):
+        return system.gen_systems_of_equations(vars, driving_var, z)
 
-    return sp.optimize.fsolve(system, x_0)
+    return sp.optimize.fsolve(function, guess)
 
-x = solution(1, five_link)
+#solves for a list of inputs, using each output as a guess for the next solution
+def create_table(inputs, solver, inital_guess, system):
+    guess = inital_guess
+    
+    outputs = np.zeros([len(inputs),len(inital_guess)])
 
-#converts radian output to degrees
-for i, angle  in enumerate(x[3:]):
-    x[i+3] = math.degrees(angle)%360
+    for i, input in enumerate(inputs):
+        outputs[i] = solver(input, guess, system)
+        guess = outputs[i]
 
-print(x)
+    return outputs
 
-#z_vals = np.linspace(-2, 2, 10)
+
+#creates a table of solutions which can be plotted and analyized 
+x_0 = np.array([ 4.48, -0.94, -2.,   -0.01, -0.05,  0.03, -0.23, -0.44, -0.18, -0.43, -0.18, -0.39, -0.23, -0.39, -0.23, -0.41])
+
+z_vals = np.linspace(-2, 2, 10)
+
+positions = create_table(z_vals, solve_z, x_0, five_link)
+
+
+print(positions)
+
