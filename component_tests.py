d6b4e10c9eaf3cade9608f1dbd76061e16b31b10
Michael Panzer <michaelpanzer04@gmail.com>
2024-11-24 11:14:24 -1000

    Wrote tests for single link and a-arm

--- component_tests.py
+++ component_tests.py
@@ -1,17 +1,36 @@
 import numpy as np
-from suspension_components import A_Arm
+from suspension_components import *
+import sympy
+import pytest
 
-def test_A_arm_rotation_generation(): 
-    #TODO add symbolic stuff here to make the test easier to fix
-    outer_ball_joint = np.array([15.0,0.0,0.0])
+def test_A_arm_rotation_length_dosent_change(): 
+    rx, ry, rz, p1x, p1y, p1z = 15, 3, 6, 0, -5, 2
 
-    a_arm = A_Arm(np.array([0,0,0]), np.array([5,6,7]), outer_ball_joint)
+    outer_ball_joint = np.array([rx, ry, rz])
 
-    rotation_mat = a_arm.local_A_matrix()
+    a_arm = A_Arm(np.array([0,0,0]), np.array([p1x, p1y, p1z]), outer_ball_joint)
 
-    final_ball_joint_pos = np.dot(rotation_mat, np.array([[np.cos(2)], [np.sin(2)]]))
+    rotation_mat, offset_vector = a_arm.local_A_matrix()
+
+    cos, sin = np.cos(2), np.sin(2)
+    final_ball_joint_pos = np.dot(rotation_mat, np.array([[cos], [sin]])) + offset_vector
 
     print(final_ball_joint_pos)
-    print(outer_ball_joint)
+    #print(outer_ball_joint)
 
-    assert np.sum(outer_ball_joint**2) == np.sum(final_ball_joint_pos**2)
+    assert np.sum(outer_ball_joint**2) == pytest.approx(np.sum(final_ball_joint_pos**2))
+
+def test_Single_Link_length_dosent_change():
+    len = 16.0
+    link = Single_Link(np.array([5,2,8]), len)
+
+    alpha = 0.5
+    beta = 4
+
+    local_A_mat = link.local_A_matrix()
+
+    nonlin_vector = link.nonlin_x_expression((alpha,beta))
+
+    final_pos = np.dot(local_A_mat, np.atleast_2d(nonlin_vector).T)
+
+    assert len == pytest.approx(np.sqrt(np.sum(final_pos**2)))
--- suspension_components.py
+++ suspension_components.py
@@ -3,11 +3,15 @@ import numpy as np
 
 class Linkage(ABC):
     @abstractmethod
-    def position_unit_vector(self):
-        pass
-    
-    @abstractmethod
     def local_A_matrix(self):
+        pass
+
+    @abstractmethod
+    def nonlin_x_expression(self, vars):
+        pass
+
+    @abstractmethod
+    def render(self, vars):
         pass
 
 #these methods might not be it, more work to be done
@@ -27,12 +31,17 @@ class Single_Link(Linkage):
         self.length = length
     
     #override
-    def position_unit_vector(self, alpha, beta):
-        return np.array([np.cos(beta)*np.sin(alpha), np.cos(beta)*np.sin(alpha)])   
-    
-    #override
     def local_A_matrix(self):
         return self.length*np.identity(3)
+    
+    #override
+    def nonlin_x_expression(self, vars):
+        alpha, beta = vars
+        return np.array([np.cos(beta)*np.cos(alpha), np.cos(beta)*np.sin(alpha), np.sin(beta)])   
+    
+    #override TODO
+    def render(self):
+        ...
 
 class A_Arm(Linkage):
     def __init__(self, frame_pickup_0, frame_pickup_1, ball_joint_pos):
@@ -40,34 +49,41 @@ class A_Arm(Linkage):
         self.frame_pickup_1 = frame_pickup_1
         self.ball_joint_pos = ball_joint_pos
 
-        self.ball_joint_distance = np.linalg.norm(self.ball_joint_pos)
-        self.ball_joint_unit_vector = self.ball_joint_pos / self.ball_joint_distance
+        pickup_1_to_0 = frame_pickup_0-frame_pickup_1
+        rotation_axis_unit_vector = np.atleast_2d(pickup_1_to_0 / np.linalg.norm(pickup_1_to_0))
+
+        #print(rotation_axis_unit_vector)
 
         #saves pivot axis as unit vector
-        pivot = frame_pickup_0, frame_pickup_1
+        pivot = frame_pickup_0 - frame_pickup_1
         self.pivot_axis = pivot / np.linalg.norm(pivot)
 
         #https://en.wikipedia.org/wiki/Rotation_matrix
-        self.cross_product_matrix = np.array([[                              0,-self.ball_joint_unit_vector[2], self.ball_joint_unit_vector[1]],
-                                              [ self.ball_joint_unit_vector[2],                              0, -self.ball_joint_unit_vector[0]],
-                                              [-self.ball_joint_unit_vector[1], self.ball_joint_unit_vector[2],                              0]])
-        self.outer_product_matrix = np.dot(self.ball_joint_unit_vector, self.ball_joint_unit_vector.T)
+        self.cross_product_matrix = np.array([[                            0, -rotation_axis_unit_vector[0,2], rotation_axis_unit_vector[0,1] ],
+                                              [ rotation_axis_unit_vector[0,2],                             0, -rotation_axis_unit_vector[0,0]],
+                                              [-rotation_axis_unit_vector[0,1],  rotation_axis_unit_vector[0,0],                             0]])
+        self.outer_product_matrix = np.dot(rotation_axis_unit_vector.T, rotation_axis_unit_vector)
 
-    #override
-    def position_unit_vector(self, alpha):
-        cos = np.cos(alpha)
-        return cos*np.identity(3) + np.sin(alpha)*self.cross_product_matrix + (1-cos)*self.outer_product_matrix
-    
-    
     #override
     def local_A_matrix(self):
         ball_joint_colum_vec = np.atleast_2d(self.ball_joint_pos).T
 
-        cos_vector = np.dot((np.identity(3)-self.outer_product_matrix), ball_joint_colum_vec)
-        sin_vector = np.dot(self.cross_product_matrix, ball_joint_colum_vec)
+        cos_colum = np.dot((np.identity(3)-self.outer_product_matrix), ball_joint_colum_vec)
+        sin_colum = np.dot(self.cross_product_matrix, ball_joint_colum_vec)
 
-        return np.block([cos_vector, sin_vector]) + np.dot(self.outer_product_matrix, ball_joint_colum_vec)
+        return np.block([cos_colum, sin_colum]), np.dot(self.outer_product_matrix, np.atleast_2d(self.ball_joint_pos).T)
+    
+    #override
+    def nonlin_x_expression(self, vars):
+        alpha = vars
 
+        return np.array([np.cos(alpha), np.sin(alpha)])
+    
+    #override TODO
+    def render(self):
+        ...
+
+#TODO add stuff for these 
 class H_Arm:...
 
 class Strut:...
