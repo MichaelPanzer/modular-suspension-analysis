Fixed tests (except for jacobian)

Tests/test_components.py
@@ -12,11 +12,11 @@ def test_A_arm_rotation_length_doesnt_change():
 
     a_arm = A_Arm(np.array([0,0,0]), np.array([p1x, p1y, p1z]), outer_ball_joint)
 
-    rotation_mat, offset_vector = a_arm.local_A_matrix()#TODO change this so rotation mat and offset vector are two different operations
+    rotation_mat = a_arm.local_A_matrix()
 
     
     cos, sin = np.cos(2), np.sin(2)
-    final_ball_joint_pos = np.dot(rotation_mat, np.array([[cos], [sin]])) + offset_vector
+    final_ball_joint_pos = np.dot(rotation_mat, np.array([[cos], [sin]])) 
 
     assert np.sum(outer_ball_joint**2) != pytest.approx(np.sum(final_ball_joint_pos**2))
 

Tests/test_independent_suspension.py
@@ -71,10 +71,10 @@ def test_jacobian():
 
     vars = np.random.rand(16)
 
-    my_jacobian = fl.jacobian(vars, 2)
+    my_jacobian = fl.jacobian(vars, [2])
 
     def func(vars):
-        return fl.full_sys_of_eq(vars, 2, np.random.rand())
+        return fl.full_sys_of_eq(vars, ([2], [np.random.rand()]))
     
     correct_jacobian = jacobi(func, vars)[0]
 

src/suspmatics/components.py
@@ -143,6 +143,11 @@ class Single_Link(Linkage):
         return self.vp_object
     
 class A_Arm(Linkage):
+    linear_input_count = 3
+    output_count = 3
+    input_names = ["alpha"]
+    input_count = len(input_names)
+    
     def __init__(self, frame_pickup_0, frame_pickup_1, ball_joint_pos):
         self.frame_pickup_0 = frame_pickup_0
         self.frame_pickup_1 = frame_pickup_1
@@ -151,8 +156,6 @@ class A_Arm(Linkage):
 
         pickup_1_to_0 = frame_pickup_0-frame_pickup_1
         rotation_axis_unit_vector = np.atleast_2d(pickup_1_to_0 / np.linalg.norm(pickup_1_to_0))
-
-        #print(rotation_axis_unit_vector)
 
         #saves pivot axis as unit vector
         pivot = frame_pickup_0 - frame_pickup_1
@@ -167,16 +170,16 @@ class A_Arm(Linkage):
         #this is the point on the pivot axis where the ball joint position vector is orthogonal to axis
         self.orthogonal_link_position = np.dot(self.outer_product_matrix, np.atleast_2d(self.ball_joint_pos).T)
 
-
     @override
     def local_A_matrix(self):
-        ball_joint_colum_vec = np.atleast_2d(self.ball_joint_pos).T
+        ball_joint_column_vec = np.atleast_2d(self.ball_joint_pos).T
 
-        cos_colum = np.dot((np.identity(3)-self.outer_product_matrix), ball_joint_colum_vec)
-        sin_colum = np.dot(self.cross_product_matrix, ball_joint_colum_vec)
+        cos_column = np.dot((np.identity(3)-self.outer_product_matrix), ball_joint_column_vec)
+        sin_column = np.dot(self.cross_product_matrix, ball_joint_column_vec)
 
-        return np.block([cos_colum, sin_colum])
+        return np.block([cos_column, sin_column])
     
+    @override
     def local_B_vector(self):
         return self.frame_pickup_0 - self.orthogonal_link_position
 
@@ -194,6 +197,14 @@ class A_Arm(Linkage):
     def jacobian(self, vars):
         alpha = vars
         return np.array([-np.sin(alpha), np.cos(alpha)]) 
+    
+    @override
+    def create_vp_object():
+        pass
+
+    @override
+    def update_vp_position():
+        pass
 
 #TODO add stuff for these 
 class H_Arm:...
@@ -241,7 +252,7 @@ class Upright(Wheel_Carrier):
     def approx_nonlin_x(self, vars):
         wheel_x, wheel_y, wheel_z, theta, phi, gamma = vars
 
-        #TODO this whole thing can be made so much more efficent
+        #TODO this whole thing can be made so much more efficient
         r_x = np.array([[1, 0, 0],
                         [0, approx_cos(theta), -1*approx_sin(theta)],
                         [0, approx_sin(theta), approx_cos(theta)]])
@@ -263,7 +274,7 @@ class Upright(Wheel_Carrier):
 
         wheel_jac = np.identity(3)
 
-        #TODO this whole thing can be made so much more efficent
+        #TODO this whole thing can be made so much more efficient
         r_x = np.array([[1, 0, 0],
                         [0, np.cos(theta), -np.sin(theta)],
                         [0, np.sin(theta), np.cos(theta)]])
@@ -300,9 +311,9 @@ class Upright(Wheel_Carrier):
         output = np.zeros(3+2*self.pickup_count, dtype=vpython.standardAttributes)
         r = diameter/2
 
-        #the first 3 objects are the basis vectors for the local frame of refrence
+        #the first 3 objects are the basis vectors for the local frame of reference
         output[0] = vpython.arrow(axis=vpython.vector(0,0,axis_len), color=vpython.color.blue)
-        output[1] = vpython.arrow(axis=vpython.vector(-axis_len,0,0), color=vpython.color.green)#wheel is axisymetric so these mfs prolly arent nessecary
+        output[1] = vpython.arrow(axis=vpython.vector(-axis_len,0,0), color=vpython.color.green)#wheel is axisymmetric so these mfs prolly aren't necessary
         output[2] = vpython.arrow(axis=vpython.vector(0,-axis_len,0), color=vpython.color.red)
 
         #All remaining objects are for the pickup points

src/suspmatics/independent_suspension.py
@@ -54,7 +54,7 @@ class Kinematic_Model:
         return np.atleast_2d(np.block(B_vector.tolist())).T
 
     def generate_x_nonlinear(self, vars):
-        x = np.zeros(27) # fix this so inital length is the sum of all the linear input counts
+        x = np.zeros(27) # fix this so initial length is the sum of all the linear input counts
 
 
         x[0:self.wheel_carrier.linear_input_count] = self.wheel_carrier.nonlin_x_expression(vars[0:self.wheel_carrier.input_count]) #wheel carrier position and rotation
@@ -110,7 +110,7 @@ class Kinematic_Model:
 
         return np.concatenate((nonlin_expressions, driving_exprs))
     
-    #this probably isnt nessecary
+    #TODO delete this
     def approx_sys_of_eq(self, vars, driving_vals):
             x = self.approx_x_nonlinear(vars)
 
@@ -124,13 +124,13 @@ class Kinematic_Model:
 
             return np.concatenate((nonlin_expressions, np.array([driving_exprs])))
     
-    #TODO write some fucking tests stoopidhead
+
     def jacobian(self, vars, driving_var_indices):
         jacobians = np.zeros(self.linkages.shape[0] + 1, dtype=np.ndarray)
         link_angles = vars[self.wheel_carrier.input_count:]
         jacobians[0] = self.wheel_carrier.jacobian(vars[0:self.wheel_carrier.input_count])
 
-        #the first index is deticated to the wheel jacobian
+        #the first index is dedicated to the wheel jacobian
         link_jacobians = jacobians[1:]
         j = 0 #link input index
         for i, link in enumerate(self.linkages):