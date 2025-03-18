77232d3b5dcd23c2bd16a540ab6f608018b71ac5
MichaelPanzer <michaelpanzer04@gmail.com>
2025-03-17 23:20:56 -1000

    created benchmark for rotation matrix generation

--- benchmarks/roation_benchmark.py
+++ benchmarks/roation_benchmark.py
@@ -1,7 +1,10 @@
+#type: ignore
 import numpy as np
 import time
+from scipy.spatial.transform import Rotation
 
-def method_a(vec: np.ndarray, angles: tuple) -> np.ndarray:
+#creates rot matrices for each axis and multiplies them together
+def ver_a(vec: np.ndarray, angles: tuple):
     phi, theta, gamma = angles
 
     r_x = np.array([[1, 0, 0],
@@ -18,6 +21,72 @@ def method_a(vec: np.ndarray, angles: tuple) -> np.ndarray:
     
     return vec.dot(r_x.dot(r_y.dot(r_z)))
 
+#same as ver a except the trig functions are calculated once and stored as vars
+def ver_b(vec: np.ndarray, angles: tuple):
+    phi, theta, gamma = angles
+    ct = np.cos(theta)
+    st = np.sin(theta)
 
-count = 10**5
+    cp = np.cos(phi)
+    sp = np.sin(phi)
 
+    cg = np.cos(gamma)
+    sg = np.sin(gamma)
+    
+    r_x = np.array([[1, 0, 0],
+        [0, ct, -st],
+        [0, st, ct]])
+        
+    r_y = np.array([[cp, 0, sp],
+        [0, 1, 0],
+        [-sp, 0, cp]])
+        
+    r_z = np.array([[cg, -sg, 0],
+        [sg, cg, 0],
+        [0, 0, 1]])
+    
+    return vec.dot(r_x.dot(r_y.dot(r_z)))
+
+#using scipy rotation class
+def ver_c(vec: np.ndarray, angles: tuple):
+    r = Rotation.from_euler("xyz", angles).as_matrix()
+    
+    return vec.dot(r)
+
+
+count = 10**6
+
+random_inputs = np.random.rand(count, 2, 3)
+
+#ver a
+start_time = time.time()
+for input in random_inputs:
+    output = ver_a(input[0], input[1])
+end_time = time.time()
+time_a = end_time-start_time
+print("a: " + str(time_a))
+
+#ver b
+start_time = time.time()
+for input in random_inputs:
+    output = ver_b(input[0], input[1])
+end_time = time.time()
+time_b = end_time-start_time
+print("b: " + str(time_b))
+
+#ver c
+start_time = time.time()
+for input in random_inputs:
+    output = ver_c(input[0], input[1])
+end_time = time.time()
+time_c = end_time-start_time
+print("c: " + str(time_c))
+
+
+def as_perc(datum, value):
+    return 100*(value/datum)
+
+
+print(as_perc(time_a, time_a))
+print(as_perc(time_a, time_b))
+print(as_perc(time_a, time_c))
