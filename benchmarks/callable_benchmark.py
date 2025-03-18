825f1e20abff4c18f2b4dc044c85a35b3c7f85ac
MichaelPanzer <michaelpanzer04@gmail.com>
2025-03-18 00:57:39 -1000

    updated callable benchmark

--- benchmarks/callable_benchmark.py
+++ benchmarks/callable_benchmark.py
@@ -24,6 +24,7 @@ count = 10**7
 
 rand_inputs = np.random.rand(count, 2)
 
+
 #method call
 m = multiplier()
 start_time = time.time()
@@ -31,7 +32,7 @@ for input in rand_inputs:
     output = m.method_call_product(input)
 end_time = time.time()
 time_method = end_time-start_time
-print("method_call: " + str(time_method))
+print("method call: " + str(time_method))
 
 
 #func call from method
@@ -40,7 +41,7 @@ for input in rand_inputs:
     output = m.callable_product()(input)
 end_time = time.time()
 time_func_from_method = end_time-start_time
-print("func_call_from_method: " + str(time_func_from_method))
+print("func call from method: " + str(time_func_from_method))
 
 
 #func call without method
@@ -50,7 +51,7 @@ for input in rand_inputs:
     output = prod(input)
 end_time = time.time()
 time_func = end_time-start_time
-print("func_call: " + str(time_func))
+print("func call: " + str(time_func))
 
 #pure function call
 start_time = time.time()
@@ -58,7 +59,18 @@ for input in rand_inputs:
     output = product(input)
 end_time = time.time()
 time_func_pure = end_time-start_time
-print("func_call: " + str(time_func_pure))
+print("pure func call: " + str(time_func_pure))
+
+
+#no calls
+m = multiplier()
+start_time = time.time()
+for input in rand_inputs:
+    a,b = input
+    output = a*b
+end_time = time.time()
+time_no_calls = end_time-start_time
+print("no calls: " + str(time_no_calls))
 
 
 
@@ -67,3 +79,4 @@ print(as_perc(time_method, time_method))
 print(as_perc(time_method, time_func_from_method))
 print(as_perc(time_method, time_func))
 print(as_perc(time_method, time_func_pure))
+print(as_perc(time_method, time_no_calls))
