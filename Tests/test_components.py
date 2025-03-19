654971903c38e306561d3e91aa801da305e694ff
MichaelPanzer <michaelpanzer04@gmail.com>
2025-03-18 15:50:54 -1000

    fixed component tests

--- Tests/test_components.py
+++ Tests/test_components.py
@@ -8,16 +8,16 @@ importlib
 
 def test_A_arm_rotation_length_doesnt_change(): 
     rx, ry, rz, p1x, p1y, p1z = 15, 3, 6, 0, -5, 2
+    angle = 0.69
 
     outer_ball_joint = np.array([rx, ry, rz])
 
     a_arm = A_Arm(np.array([0,0,0]), np.array([p1x, p1y, p1z]), outer_ball_joint)
 
-    rotation_mat = a_arm.local_coef_mat()
+    coef_mat = a_arm.local_coef_mat()
+    nonlin_vec = a_arm.nonlin_expression()(angle)
 
-    
-    cos, sin = np.cos(2), np.sin(2)
-    final_ball_joint_pos = np.dot(rotation_mat, np.array([[cos], [sin]])) 
+    final_ball_joint_pos = np.dot(coef_mat, nonlin_vec) 
 
     assert np.sum(outer_ball_joint**2) != pytest.approx(np.sum(final_ball_joint_pos**2))
 
@@ -30,7 +30,7 @@ def test_Single_Link_length_doesnt_change():
 
     local_A_mat = link.local_coef_mat()
 
-    nonlin_vector = link.nonlin_x_expression((alpha,beta))
+    nonlin_vector = link.nonlin_expression()((alpha,beta))
 
     final_pos = np.dot(local_A_mat, np.atleast_2d(nonlin_vector).T)
 
@@ -46,7 +46,7 @@ def test_Upright_lengths_dont_change():
 
     local_A = upright.local_coef_mat()
 
-    x = upright.nonlin_x_expression((0,0,0, theta, phi, gamma))
+    x = upright.nonlin_expression()((0,0,0, theta, phi, gamma))
 
     assert np.sum(np.dot(local_A, x)**2) == pytest.approx(np.sum(upright_pickups**2))
 
